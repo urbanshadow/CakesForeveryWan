1b20ece74811b945630adc99ec97b1b0ff083a6b
urbanshadow <urbanshado08@gmail.com>
2016-04-02 11:27:54 +0200

    Fix to arm9hook issue #1
    Fixes issue of arm9hook #1
    -Removes return address patching
    Cleaned code

--- source/a9hook.c
+++ source/a9hook.c
@@ -3,20 +3,6 @@
 #include "draw.h"
 #include "fs.h"
 
-struct firm_hook firm_hooks[] = {
-	{
-		.version = 0x50,
-		.p9return = 0x0808604C
-	},
-	{
-		.version = 0x1B,
-		.p9return = 0x080860A0
-	},
-	{
-		.version = 0xFF,
-		.p9return = 0
-	}
-};
 
 int load_arm9_hook(unsigned int version){
 	print("Copying hook to itcm...");
@@ -25,15 +11,7 @@ int load_arm9_hook(unsigned int version){
         //draw_message("Failed to load arm9hook", "Please make sure arm9hook file\n  actually exists on the SD card.");
         return 1;
     }
-	
-	unsigned int i;
-	
-	for(i=0;firm_hooks[i].version != 0xFF;i++){
-		if(version == firm_hooks[i].version) {
-			*((unsigned int*)A9H_JUMPBACK) = firm_hooks[i].p9return;
-			break;
-		}
-	}
+
 	print("Hook copied succesfully!");
 	
 	return 0;
--- source/a9hook.h
+++ source/a9hook.h
@@ -1,9 +1,3 @@
-#define A9H_JUMPBACK 0x01FFB63C
 #define A9H_ITCM_POS 0x01FFB638
-
-struct firm_hook {
-    unsigned int version;
-    unsigned int p9return;
-};
 
 int load_arm9_hook(unsigned int);
